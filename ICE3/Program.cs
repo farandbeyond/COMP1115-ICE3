09876571843a2a63ab41cede381084b24d1e6852
Connor Hadley <connorjh4@gmail.com>
2025-09-19 16:39:02 -0400

    Used if/else to determine primary and secondary objectives

--- ICE3/Program.cs
+++ ICE3/Program.cs
@@ -11,7 +11,7 @@
             //secondary attributes
             int Awareness = 0, Toughness = 0, Resolve = 0;
             //career variables
-            string Career = "";
+            string Career = "Unknown";
             bool HasChosenCareer = false;
 
             //setup career selection
@@ -25,6 +25,83 @@
             Console.WriteLine("3: Telepath");
             Console.WriteLine("4: Rogue");
             Console.WriteLine("5: Tinker");
+
+            //take the users input and choose their job
+            int careerChoice = Convert.ToInt32(Console.ReadLine());
+            if(careerChoice == 1)
+            {
+                Career = "Army";
+
+                Agility = 20;
+                Strength = 50;
+                Vigour = 40;
+                Perception = 30;
+                Intellect = 5;
+                Will = 10;
+
+                HasChosenCareer = true;
+            }else if(careerChoice == 2)
+            {
+                Career = "Psion";
+
+                Agility = 30;
+                Strength = 5;
+                Vigour = 10;
+                Perception = 20;
+                Intellect = 40;
+                Will = 50;
+
+                HasChosenCareer = true;
+            }
+            else if(careerChoice == 3)
+            {
+                Career = "Telepath";
+
+                Agility = 5;
+                Strength = 10;
+                Vigour = 30;
+                Perception = 50;
+                Intellect = 20;
+                Will = 40;
+
+                HasChosenCareer = true;
+            }
+            else if(careerChoice == 4)
+            {
+                Career = "Rogue";
+
+                Agility = 50;
+                Strength = 30;
+                Vigour = 20;
+                Perception = 40;
+                Intellect = 10;
+                Will = 5;
+
+                HasChosenCareer = true;
+            }
+            else if(careerChoice == 5)
+            {
+                Career = "Tinker";
+
+                Agility = 40;
+                Strength = 10;
+                Vigour = 30;
+                Perception = 5;
+                Intellect = 50;
+                Will = 20;
+
+                HasChosenCareer = true;
+            }
+            else
+            {
+                //if users choice is invalid, tell them
+                Console.WriteLine("That is not a valid job option.");
+            }
+
+            //determine secondary attributes
+            Awareness = Agility + Perception;
+            Toughness = Strength + Vigour;
+            Resolve = Intellect + Will;
 
             Console.ReadKey(true);
 
